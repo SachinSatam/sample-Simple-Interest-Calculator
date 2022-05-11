d5ac5f7b615a35348110929d147bcd00dd3a8ae2
Sachin Satam <sachinsatam4@gmail.com>
2022-05-12 01:14:06 +0530

    Commit 2

--- script.js
+++ script.js
@@ -15,16 +15,21 @@ function compute()
     var output=document.getElementById("value");
     var amount1
     var amount2
-    var princ=parseInt(document.getElementById("principal").value);
+    var principal=parseInt(document.getElementById("principal").value);
     var gettext=parseInt(document.getElementById("select1").value);
     var finyear
-    amount1=(princ*output.innerHTML)/100  
+    if(principal<=0 || !principal)
+    {
+        alert("ERROR :(  Enter a positive integer")
+        principal.focus()
+    }    
+    amount1=(principal*output.innerHTML)/100  
     amount2=amount1*gettext;
     finyear=2020+gettext;
-    document.getElementById("d").innerHTML=(`If you deposit ${princ}`);
-    document.getElementById("e").innerHTML=(`at an interest rate of ${output.innerHTML}`);
-    document.getElementById("f").innerHTML=(`You will receive an amount of ${amount2}`);
-    document.getElementById("g").innerHTML=(`in the year ${finyear}`);
+    document.getElementById("d").innerHTML=(`If you deposit <mark>${principal}</mark>`);
+    document.getElementById("e").innerHTML=(`at an interest rate of <mark>${output.innerHTML}</mark>`);
+    document.getElementById("f").innerHTML=(`You will receive an amount of <mark>${amount2}</mark>`);
+    document.getElementById("g").innerHTML=(`in the year <mark>${finyear}</mark>`);
     
                                                           
 }        
